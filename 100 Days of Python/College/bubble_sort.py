bcb2ebd19747125207d770cf682c8cc76998a0ba
DishanSamuel <dishansamuels@gmail.com>
2023-12-31 00:16:54 +0530

    binary search complete

---
+++ 100 Days of Python/College/bubble_sort.py
@@ -0,0 +1,32 @@
+global list
+list=[]
+def swap(n):
+    temp = list[n]
+    list[n] = list[n+1]
+    list[n+1] = temp
+def input_numbers():
+    numbers = int
+    input_code = 0
+    loop = True
+    while loop:
+        input_code = input('Enter the numbers to sort : ')
+        if input_code == 'end':
+            loop = False
+            break
+        elif input_code != 'end':
+            try:
+                numbers = int(input_code)
+            except ValueError:
+                print("Invalid input")
+                break
+            list.append(numbers)
+def main():
+    for i in range(0, len(list) - 1):
+        for j in range(0, len(list) - 1-i):
+            if list[j] > list[j+1]:
+                swap(j)
+
+input_numbers()
+print(list)
+main()
+print(list)
